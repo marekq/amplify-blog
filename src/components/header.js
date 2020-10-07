d6bc9452a17f10cea30d2c9c5fdef63c86771e42
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-10-08 01:01:31 +0200

    CSS fix, UI fixes

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./css/header.module.css"
+import bootstrap from "./css/bootstrap.min.css"
 
 const Header = () => (
   <header className={styles.header}>
+    <link rel="stylesheet" href={bootstrap} />
+
     <div className={styles[`header__wrap`]}>
-      <h2>
-        <Link
-          to="/"
-          className={styles[`header__link`]}
-        >
+      <nav role="main" className={styles[`header__nav`]}>
+        <Link to="/" className={styles[`header__link`]}>
           Home
         </Link>
-      </h2>
-      <nav role="main" className={styles[`header__nav`]}>
         <Link to="/app/all" className={styles[`header__link`]}>
-          AWS Blog
+          Blog
         </Link>
         <Link to="/app/marek" className={styles[`header__link`]}>
           Marek
