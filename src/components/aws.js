5a7df68abf78049761f6098f0d33c869ae24aadd
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-10-08 23:36:41 +0200

    added compact mode to aws blog view

--- src/components/aws.js
+++ src/components/aws.js
@@ -19,17 +19,19 @@ class App extends React.Component {
 
 		// to be improved - get the uri of the url by stripping /app from the url
 		var bloguri = props.path.slice(5, 999);
-		this.state = { url1: url + bloguri + '.json' };
+		this.state = { url1: url + bloguri + '.json', compact: true };
 	}
 
 	render() {
 		return (
-			<div className = "container" width = "1200px">
+			<div className = "container">
 				
 				<Async promiseFn = {loadBlogs} blogUrl = {this.state.url1}>
 				<Async.Loading>Loading... </Async.Loading>
 				<Async.Fulfilled>
 					{data => {
+
+						console.log(this.state.compact);
 
 						// get the current time
 						var now = new Date().getTime();
@@ -58,28 +60,47 @@ class App extends React.Component {
 						});
 
 						// set the table fields of the aws blog post table
-						const fields = [
-							{ name: 'timest', visible: false },
-							{ name: 'datestr', displayName: "Age", visible: true },
-							{ name: 'blogsource', displayName: "Blog", inputFilterable: true, exactFilterable: false, sortable: true },
-							{ name: 'title', displayName: "Title", inputFilterable: true, exactFilterable: false, sortable: true },
-							{ name: 'link', inputFilterable: false, visible: false }
-						];
+						const fields = [];
+						const viewswitch = [];
 
-						// add description field if the page is shown on a device with larger resolution
-						if (window.innerWidth > 1000) {
+						// add description and datestr field if the compact view state is false
+						if (this.state.compact !== true) {
+							fields.push({ name: 'timest', visible: false })
+							fields.push({ name: 'datestr', displayName: "Age", visible: true })
+							fields.push({ name: 'blogsource', displayName: "Blog", inputFilterable: true, exactFilterable: false, sortable: true })
+							fields.push({ name: 'title', displayName: "Title", inputFilterable: true, exactFilterable: false, sortable: true})
+							fields.push({ name: 'link', inputFilterable: false, visible: false })
 							fields.push({ name: 'description', displayName: "Description", inputFilterable: true, exactFilterable: false })
-						};
+						
+						// do not add description and datestr field if the compact view state is true
+						} else {
+
+							fields.push({ name: 'timest', visible: false })
+							fields.push({ name: 'blogsource', displayName: "Blog", inputFilterable: true, exactFilterable: false, sortable: true })
+							fields.push({ name: 'title', displayName: "Title", inputFilterable: true, exactFilterable: false, sortable: true})
+							fields.push({ name: 'link', inputFilterable: false, visible: false })
+
+						}
+
+						if (window.innerWidth > 1000) {
+							viewswitch.push(<Link onClick = {() => {this.setState({ compact: true })}} to = '.'><i>Compact View</i></Link>);
+							viewswitch.push(<Link onClick = {() => {this.setState({ compact: false })}} to = '.'><i>Full Mode</i></Link>);
+
+						}
 
 						return (
 							<div>
 								<center>
+								{viewswitch}
+
 								<br /><br />
+
 								<Link to = "/app/all/">All blogs</Link>{' • '}
 								<Link to = "/app/whats-new/">What's New</Link>{' • '}
 								<Link to = "/app/compute/">Compute</Link>{' • '}
 								<Link to = "/app/developer/">Developer</Link>{' • '}
 								<Link to = "/app/mobile/">Mobile</Link>
+								<br />
 
 								<FilterableTable
 									namespace="blogs"
