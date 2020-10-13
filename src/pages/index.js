0bbe657f74647c0638b52a74ed448d719bda2a63
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-10-13 22:35:22 +0200

    added page title, updated screenshot

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
 import View from "../components/view"
 import Header from "../components/header"
+import Helmet from 'react-helmet';
+
+// set page title
+var pageTitle = 'Serverless Blog';
 
 const Index = () => (
   <View>
     <Header />
+    <Helmet>
+        <title>{pageTitle}</title>
+    </Helmet>
     <div id = "container">
         <br />
         <center>
