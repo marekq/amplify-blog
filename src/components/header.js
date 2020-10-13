709e807ac5d6e3d87825a754f935098a2be7416a
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-10-13 23:02:38 +0200

    added compact and cleaner menu, cleaned css files

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
-import styles from "./css/header.module.css"
 import bootstrap from "./css/bootstrap.min.css"
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const Header = () => (
-  <header className={styles.header}>
+  <header>
     <link rel="stylesheet" href={bootstrap} />
 
-    <div className={styles[`header__wrap`]}>
-      <nav role="main" className={styles[`header__nav`]}>
-        <Link to="/" className={styles[`header__link`]}>
-          Home
-        </Link>
-        <Link to="/app/all" className={styles[`header__link`]}>
-          Blog
-        </Link>
-        <Link to="/app/marek" className={styles[`header__link`]}>
-          Marek
-        </Link>
-        <Link to="/app/about" className={styles[`header__link`]}>
-          About
-        </Link>
-      </nav>
+    <div className = "container">
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">     
+        <img
+          src="/favicon.ico"
+          width="20"
+          height="20"
+          alt="logo"
+        />
+        {'  '}marek.rocks
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav"  />
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/app/all">Blog</Nav.Link>
+        <Nav.Link href="/app/about">About</Nav.Link>
+        <Nav.Link href="/app/marek">Marek</Nav.Link>
+      </Nav>
+    </Navbar>
     </div>
   </header>
 )
