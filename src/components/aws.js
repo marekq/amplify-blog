92e7fdd9f8cf42a86c75653afff511b409ed60f0
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-10-25 23:10:52 +0100

    removed async load module for smoother browsing

--- src/components/aws.js
+++ src/components/aws.js
@@ -1,5 +1,4 @@
 import React, { forwardRef } from 'react'
-import Async from 'react-async';
 import prettyMilliseconds from 'pretty-ms';
 import fetch from 'node-fetch';
 import { Link } from "gatsby";
@@ -27,13 +26,7 @@ const tableIcons = {
 	PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
 	ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
 	Search: forwardRef((props, ref) => <Search {...props} ref={ref} />)
-  };
-
-// load the blog from s3
-const loadBlogs = ({ blogUrl }) =>
-  fetch(blogUrl)
-	.then(res => (res.ok ? res : Promise.reject(res)))
-	.then(res => res.json())
+};
 
 // main react class
 class App extends React.Component {
@@ -46,6 +39,7 @@ class App extends React.Component {
 
 		// set the state of url, path and sidebar status
 		this.state = { url1: url + bloguri + '.json', path1: String(bloguri), sidebarOpen: false, sidebarDocked: mql.matches };
+
 		this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
 		this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
 	}
@@ -53,6 +47,36 @@ class App extends React.Component {
 	componentWillMount() {
 		mql.addListener(this.mediaQueryChanged);
 	}
+
+	// load the blog from s3
+	async componentDidMount(){
+		var res = await fetch(this.state.url1)
+		var data = await res.json()
+		
+		// get the current timestamp
+		var now = new Date().getTime();
+
+		// convert the unix timestamp of every blog to a timediff string
+		data.map(function(blog, index){
+							
+			// get the time difference in seconds
+			var timestamp = now - (blog.timest * 1000);
+
+			// get the time difference string and set it in data
+			var timediff = prettyMilliseconds(timestamp, {compact: true});
+			blog.datestr = timediff;
+			
+			// strip dashes from blog source and add link
+			blog.bloglink = <Link to = {`/app/${blog.blogsource}`}>{blog.blogsource.toString().replace("-", " ")}</Link>
+
+			return '';
+
+		});
+
+		this.setState({
+			data: data
+		})
+	  }
 
 	mediaQueryChanged() {
 		this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
@@ -63,147 +87,117 @@ class App extends React.Component {
 	}
 
 	render() {
+		const fields = [];
+		const tmpurl = this.state.url1;
+		const returnlink = [];
+
+		if (mql) {
+			fields.push({ title: 'Timest', field: 'timest', defaultSort: 'desc', hidden: true, searchable: false});
+			fields.push({ title: 'Age', field: 'datestr', width: 10, searchable: true });
+
+			// add the blogsource if the 'all' category is selected
+			if (tmpurl.endsWith("all.json")) {
+				fields.push({ title: 'Blog', field: 'bloglink', width: 10, searchable: true })
+			} else {
+				returnlink.push(<center><Link to = "/app/all/">Go to all blogs</Link><br /><br /></center>)
+			}
+
+			fields.push({ title: 'Title', field: 'title', minwidth: 1000, searchable: true });
+
+		// if fullmode is false, do NOT add description and datestr field if the compact view state is true
+		} else {
+			fields.push({ title: 'Timest', field: 'timest', width: 10, defaultSort: 'desc', hidden: true, searchable: false});
+
+			if (tmpurl === "all") {
+				fields.push({ title: 'Blog', field: 'bloglink', width: 10, searchable: true })
+
+			} else {
+				fields.push({ title: 'Age', field: 'datestr', width: 10, searchable: true});
+				returnlink.push(<center><Link to = "/app/all/">Go to all blogs</Link><br /><br /></center>);
+			}
+
+			fields.push({ title: 'Title', field: 'title', searchable: true });
+		}
+
+		// add the description as non visible to allow search 
+		fields.push({ title: 'Description', field: 'description', hidden: true, searchable: true});
+
+		// create sidebar menu
+		const sidebar = [];
+		sidebar.push(<p key = "close"><br /><Link key = "menuClose" onClick = {() => {this.setState({ sidebarOpen: false })}} to = '.'><i><b>close menu</b></i></Link></p>);
+
+		// create a list of blog categories for the menu
+		const blogpaths = ['all', 'cloudguru', 'compute', 'corey', 'containers', 'database', 'devops', 'jeremy', 'ml', 'mobile', 'newsblog', 'open-source', 'security', 'serverless', 'whats-new', 'yan'];
+
+		for (const [index, value] of blogpaths.entries()) {
+
+			sidebar.push(<p key = {value}><Link to = {`/app/${value}`} size = "sm" variant = "secondary" key = {index}><i>{value}</i></Link></p>)
+		}
+
 		return (
 			<div className = "container">
 				
-				<Async promiseFn = {loadBlogs} blogUrl = {this.state.url1}>
-				<Async.Loading>Loading page... </Async.Loading>
-				<Async.Fulfilled>
-					{data => {
+				<Sidebar
+					sidebar = {sidebar}
+					open = {this.state.sidebarOpen}
+					onSetOpen = {this.onSetSidebarOpen}
+					onSetSidebarOpen = {this.onSetSidebarOpen}
+					styles = {{ sidebar: { background: "white", textAlign: "left", padding: "10px", width: "150px"}}}
+					transitions = {true}
+					shadow = {false}
+				>
+				<View title = "">  
+					<Header />
+					<br />								
+					<center>
+						<h3>{this.state.path1.replace('-', ' ')} blogs</h3><br />
+						{returnlink}
+					</center>
 
-						// get the current time
-						var now = new Date().getTime();
-
-						// get the url value
-						var tmpurl = this.state.url1;
-
-						// convert the unix timestamp of every blog to a timediff string
-						data.map(function(blog, index){
-							
-							// get the time difference in seconds
-							var timestamp = now - (blog.timest * 1000);
-
-							// get the time difference string and set it in data
-							var timediff = prettyMilliseconds(timestamp, {compact: true});
-							blog.datestr = timediff;
-							
-							// strip dashes from blog source and add link
-							blog.bloglink = <Link to = {`/app/${blog.blogsource}`}>{blog.blogsource.toString().replace("-", " ")}</Link>
-
-							return '';
-
-						});
-
-						// set the table fields of the aws blog post table and the viewswitch option
-						const fields = [];
-						
-						// if fullmode is true, add description and datestr field if the compact view state is false
-						if (mql) {
-							fields.push({ title: 'Timest', field: 'timest', defaultSort: 'desc', hidden: true, searchable: false});
-							fields.push({ title: 'Age', field: 'datestr', width: 20, searchable: true });
-
-							// add the blogsource if the 'all' category is selected
-							if (tmpurl.endsWith("all.json")) {
-								fields.push({ title: 'Blog', field: 'bloglink', width: 20, searchable: true })
-							};
-
-							fields.push({ title: 'Title', field: 'title', minwidth: 1000, searchable: true });
-
-						// if fullmode is false, do NOT add description and datestr field if the compact view state is true
-						} else {
-							fields.push({ title: 'Timest', field: 'timest', width: 10, defaultSort: 'desc', hidden: true, searchable: false});
-			
-							if (tmpurl === "all") {
-								fields.push({ title: 'Blog', field: 'bloglink', width: 10, searchable: true })
-
-							} else {
-								fields.push({ title: 'Age', field: 'datestr', width: 10, searchable: true});
+					<MaterialTable
+						title = {<Link to = "." onClick={() => this.onSetSidebarOpen(true) }><Menu /><br /></Link>}
+						style = {{position: "sticky", padding: "0%" }}
+						isLoading = {false}
+						padding = 'dense'
+						options = {{
+							search: true,
+							sorting: true,
+							pageSize: 50,
+							pageSizeOptions: [50, 100, 1000]
+						}}
+						filtering = {true}
+						data = {this.state.data}
+						icons = {tableIcons}
+						columns = {fields}
+						detailPanel = {[
+							{
+								tooltip: 'Show blogpost details',
+								icon: KeyboardArrowRight,
+								openIcon: KeyboardArrowDown,
+								render: data => {
+									return (
+										<div id = "container" style = {{
+											fontSize: 16,
+											margin: 20,
+											fontFamily: "-apple-system,BlinkMacSystemFont,Roboto,Arial",
+											color: 'black'
+										}}>
+											<center>
+												<i>Posted {data.datestr} ago by {data.author} in {data.bloglink}</i>
+												<br /><br />
+													{data.description}
+												<br /><br />
+												<a href = {data.link} target = "_blank" rel = "noreferrer"><b>Visit blog here</b></a>
+											</center>
+										</div>
+									)
+								},
 							}
-
-							fields.push({ title: 'Title', field: 'title', searchable: true });
-
-						}
-
-						// add the description as non visible to allow search 
-						fields.push({ title: 'Description', field: 'description', hidden: true, searchable: true});
-
-						// create sidebar menu
-						const sidebar = [];
-						sidebar.push(<p key = "close"><br /><Link key = "menuClose" onClick = {() => {this.setState({ sidebarOpen: false })}} to = '.'><i><b>close menu</b></i></Link></p>);
-
-						// create a list of blog categories for the menu
-						const blogpaths = ['all', 'cloudguru', 'compute', 'corey', 'containers', 'database', 'devops', 'jeremy', 'ml', 'mobile', 'newsblog', 'open-source', 'security', 'serverless', 'whats-new', 'yan'];
-
-						for (const [index, value] of blogpaths.entries()) {
-
-							sidebar.push(<p key = {value}><Link to = {`/app/${value}`} size = "sm" variant = "secondary" key = {index}><i>{value}</i></Link></p>)
-						}
-
-						return (
-
-							<Sidebar
-								sidebar = {sidebar}
-								open = {this.state.sidebarOpen}
-								onSetOpen = {this.onSetSidebarOpen}
-								onSetSidebarOpen = {this.onSetSidebarOpen}
-								styles = {{ sidebar: { background: "white", textAlign: "left", padding: "20px"}}}
-								transitions = {true}
-								shadow = {false}
-							>
-							<View title = "">  
-								<Header />
-								<br />								
-								<center>
-									<h1>{this.state.path1.replace('-', ' ')} blogs</h1><br />
-								</center>
-
-								<MaterialTable
-									title = {<Link to = "." onClick={() => this.onSetSidebarOpen(true) }><Menu /><br /></Link>}
-									style = {{position: "sticky", padding: "0px" }}
-									options = {{
-										search: true,
-										sorting: true,
-										pageSize: 50,
-										pageSizeOptions: [50, 100, 1000]
-									}}
-									filtering = {true}
-									data = {data}
-									icons = {tableIcons}
-									columns = {fields}
-									detailPanel = {[
-										{
-											tooltip: 'Show blogpost details',
-											icon: KeyboardArrowRight,
-											openIcon: KeyboardArrowDown,
-											render: data => {
-												return (
-													<div id = "container" style = {{
-														fontSize: 16,
-														margin: 20,
-														fontFamily: "-apple-system,BlinkMacSystemFont,Roboto,Arial",
-														color: 'black'
-													}}>
-														<center>
-															<i>Posted {data.datestr} ago by {data.author} in {data.bloglink}</i>
-															<br /><br />
-																{data.description}
-															<br /><br />
-															<a href = {data.link} target = "_blank" rel = "noreferrer"><b>Visit blog here</b></a>
-														</center>
-													</div>
-												)
-											},
-										}
-									]}
-								/>
-								</View>
-							</Sidebar>
-						)}}
-					</Async.Fulfilled>
-					<Async.Rejected>
-						Something went wrong, <Link to ="/">go home</Link>
-					</Async.Rejected>
-				</Async>
+						]}
+					/>
+					</View>
+				</Sidebar>
+						
 			</div>
 		);
 	}
