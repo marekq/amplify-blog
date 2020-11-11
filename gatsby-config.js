c222f6eccd21f3f448a25cbdc65312589caf45c4
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-11-11 20:36:54 +0100

    package and UI fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    "@babel/plugin-transform-react-jsx-source",
     {
       resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const Index = () => (
               Welcome the personal page of Marek Kuczynski, a Serverless Solutions Architect at AWS.
             </p>
             <br />
-            <img src = {'/architecture.png'} alt = "Architecture" width = "100%" />
+            <img src = {'/amplify.png'} alt = "Architecture" width = "100%" />
             <br />
             <p>
               This page is hosted on AWS Amplify and retrieves blog posts from JSON objects stored on S3. On the backend, a Step Function retieves AWS blogs every few minutes and stores the output on S3 and in DynamoDB.
