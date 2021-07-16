91a68d6dccbe87f31a511d920820bef19c1fb7d5
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-07-16 19:47:45 +0200

    remove webpack

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  plugins: [
-    {
-      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
-      options: {
-        analyzerMode: "static",
-        reportFilename: "report.html"
-      }
-    }
-  ]
+  plugins: []
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
       },
       fallback: {
         crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
+        stream: require.resolve('stream-browserify')
       },
     },
   },
